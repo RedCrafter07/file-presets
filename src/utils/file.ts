0f74091340854eb458149ffcacdc8f4f6caa4d4d
RedCrafter07 <github@redcrafter07.de>
2022-03-24 16:32:46 +0100

    File utils

---
+++ src/utils/file.ts
@@ -0,0 +1,53 @@
+import { readFileSync } from 'fs';
+import * as vscode from 'vscode';
+import { files as presets } from '../presets.json';
+
+export default async function file() {
+	const folders = vscode.workspace.workspaceFolders;
+	if (folders) {
+		let watcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(folders[0], '*'));
+		watcher.onDidCreate(async uri => {
+			let presetsFileDir: string | string[] = __dirname.split('\\');
+			presetsFileDir.pop();
+			presetsFileDir.pop();
+			presetsFileDir = presetsFileDir.join('\\') + '\\src';
+
+			console.log(`created ${uri.fsPath}`);
+
+			const fileParts = uri.fsPath.split('\\');
+			const fileName = fileParts[fileParts.length - 1];
+
+			const preset = presets.filter(p => p.files.includes(fileName))[0];
+
+			if (!preset) {
+				return;
+			}
+			vscode.window
+				.showQuickPick([ `Use preset for ${fileName}`, 'Ignore' ], {
+					canPickMany: false
+				})
+				.then(async v => {
+					switch (v) {
+						case `Use preset for ${fileName}`:
+							const workspaceEdit = new vscode.WorkspaceEdit();
+
+							const file = await readFileSync(
+								presetsFileDir + preset.presetFile.replace('.', '').replace('/', '\\')
+							);
+
+							workspaceEdit.set(uri, [
+								new vscode.TextEdit(
+									new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, 0)),
+									file.toString()
+								)
+							]);
+							vscode.workspace.applyEdit(workspaceEdit);
+							vscode.workspace.saveAll(false);
+							break;
+						default:
+							break;
+					}
+				});
+		});
+	}
+}
