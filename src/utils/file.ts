3259bf5ccbe52a8200e413027ddac8782222ade8
RedCrafter07 <github@redcrafter07.de>
2022-03-26 14:19:28 +0100

    Add file ending preset

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -2,10 +2,22 @@ import { readFileSync } from 'fs';
 import * as vscode from 'vscode';
 import { files as presets } from '../presets.json';
 
+type Preset =
+	| {
+			files: string[];
+			presetFile: string;
+			endings?: undefined;
+		}
+	| {
+			files: string[];
+			endings: string[];
+			presetFile: string;
+		};
+
 export default async function file() {
 	const folders = vscode.workspace.workspaceFolders;
 	if (folders) {
-		let watcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(folders[0], '*'));
+		let watcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(folders[0], '**'));
 		watcher.onDidCreate(async uri => {
 			let presetsFileDir: string | string[] = __dirname.split('\\');
 			if (presetsFileDir.includes('utils')) {
@@ -18,40 +30,72 @@ export default async function file() {
 
 			const fileParts = uri.fsPath.split('\\');
 			const fileName = fileParts[fileParts.length - 1];
+			const fileEnding = fileName.split('.').pop() || '';
 
-			const preset = presets.filter(p => p.files.includes(fileName))[0];
+			console.log(fileEnding);
 
-			if (!preset) {
-				return;
+			const preset: Preset = presets.filter(p => p.files.includes(fileName))[0];
+			const endingPreset: Preset = presets.filter(p => p.endings && p.endings.includes(fileEnding))[0];
+
+			if (preset && endingPreset) {
+				const options = [
+					`Use normal preset (${preset.presetFile.split('/').pop()})`,
+					`Use file ending preset (${endingPreset.presetFile.split('/').pop()})`,
+					`Ignore`
+				];
+				vscode.window
+					.showQuickPick(options, {
+						title: 'A preset for file ending and a file preset detected matching',
+						canPickMany: false,
+						ignoreFocusOut: true
+					})
+					.then(async s => {
+						switch (s) {
+							case options[0]:
+								applyPreset(uri, preset, presetsFileDir as string);
+								break;
+							case options[1]:
+								applyPreset(uri, endingPreset, presetsFileDir as string);
+								break;
+							default:
+								break;
+						}
+					});
+			} else if (preset) {
+				showPreset(uri, fileName, preset, presetsFileDir);
+			} else if (endingPreset) {
+				showPreset(uri, fileName, endingPreset, presetsFileDir);
 			}
-
-			vscode.window
-				.showQuickPick([ `Use preset for ${fileName}`, 'Ignore' ], {
-					canPickMany: false
-				})
-				.then(async v => {
-					switch (v) {
-						case `Use preset for ${fileName}`:
-							const workspaceEdit = new vscode.WorkspaceEdit();
-
-							const file = await readFileSync(
-								presetsFileDir + preset.presetFile.replace('.', '').replace('/', '\\')
-							);
-
-							workspaceEdit.set(uri, [
-								new vscode.TextEdit(
-									new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, 0)),
-									file.toString()
-								)
-							]);
-
-							vscode.workspace.applyEdit(workspaceEdit);
-							vscode.workspace.saveAll(false);
-							break;
-						default:
-							break;
-					}
-				});
 		});
 	}
+}
+
+function showPreset(uri: vscode.Uri, fileName: string, preset: Preset, presetsFileDir: string) {
+	vscode.window
+		.showQuickPick([ `Use preset for ${fileName}`, 'Ignore' ], {
+			canPickMany: false,
+			ignoreFocusOut: true
+		})
+		.then(async v => {
+			switch (v) {
+				case `Use preset for ${fileName}`:
+					await applyPreset(uri, preset, presetsFileDir);
+					break;
+				default:
+					break;
+			}
+		});
+}
+
+async function applyPreset(uri: vscode.Uri, preset: Preset, presetsFileDir: string) {
+	const workspaceEdit = new vscode.WorkspaceEdit();
+
+	const file = await readFileSync(presetsFileDir + preset.presetFile.replace('.', '').replace('/', '\\'));
+
+	workspaceEdit.set(uri, [
+		new vscode.TextEdit(new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, 0)), file.toString())
+	]);
+
+	vscode.workspace.applyEdit(workspaceEdit);
+	vscode.workspace.saveAll(false);
 }
